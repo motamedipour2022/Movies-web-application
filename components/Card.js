6d0118b01fc48103e4324d98d0a7c491063c1778
Mohsen Motamedipour <m.motamedipour2022@yahoo.com>
2022-11-12 23:45:26 +0330

    complete projects

--- components/Card.js
+++ components/Card.js
@@ -4,15 +4,14 @@ import {ThumbUpIcon} from "@heroicons/react/outline";
 export default function Card({result}) {
   const BASE_URL = "http://image.tmdb.org/t/p/original";   
   return (
-        <div>
-            <Image
-             layout="responsive" src={BASE_URL + result.backdrop_path} width={200} height={100}/>
-            <div>
-                <p className="truncate">{result.overview}</p>
-                <h2>{result.title || result.name}</h2>
-                <p>
+        <div className="p-2 hover:text-white active:text-red-500 lg:hover:scale-105 transition-transform duration-200">
+            <Image layout="responsive" src={BASE_URL + result.backdrop_path} width={200} height={100}/>
+        <div className="p-2">
+                <p className="truncate text-lg">{result.overview}</p>
+                <h2 className="text-lg font-bold">{result.title || result.name}</h2>
+                <p className="flex items-center ">
                   {result.release_date || result.first_air_date}
-                  <ThumbUpIcon className="h-5" />
+                  <ThumbUpIcon className="h-5 ml-3 mr-2" />
                   {result.voit_count}
                 </p>
                 
